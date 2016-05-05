271fbb9bcbd75490f99c308e8a32c6a078574943
Alex Caudill <hypoalex@users.noreply.github.com>
2016-05-05 04:08:26 -0500

    Switch to conda

--- macsetup.sh
+++ macsetup.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-echo "Bootstrapping pkgsrc..."
+echo "Installing conda..."
 
-curl -Os https://pkgsrc.joyent.com/packages/Darwin/bootstrap/bootstrap-trunk-x86_64-20160211.tar.gz
-sudo tar -zxpf bootstrap-trunk-x86_64-20160211.tar.gz -C /
-rm bootstrap-trunk-x86_64-20160211.tar.gz
-sudo chown -R $(whoami) /opt/pkg && sudo chown -R $(whoami) /var/db/pkgin
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > conda.sh
+bash conda.sh -f -b -p $HOME/conda && rm conda.sh
 
-echo "Installing pkgsrc packages..."
+echo "Creating conda environment..."
 
-/opt/pkg/bin/pkgin -y update
-/opt/pkg/bin/pkgin in gcc5-5.3.0nb1 qt4-4.8.7 python35-3.5.1nb2 py35-pip-7.1.2 zeromq-4.1.4 py35-qt4-4.11.4nb2 py35-sqlite3-3.5.1nb6 py35-py2app-0.9
+conda install --yes \
+  notebook \
+  ipywidgets \
+  pandas \
+  numexpr \
+  matplotlib \
+  scipy \
+  seaborn \
+  scikit-learn \
+  scikit-image \
+  sympy \
+  cython \
+  patsy \
+  statsmodels \
+  cloudpickle \
+  dill \
+  numba \
+  bokeh \
+  h5py \
+  terminado \
+  networkx \
+  django \
+  && conda clean -tipsy
 
-ln -s /opt/pkg/gcc5/bin/gfortran /opt/pkg/bin/gfortran
+pip install deap scoop sphinx flask feather-format tox
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/mac/tensorflow-0.8.0-py3-none-any.whl
 
-echo "Installing python packages..."
-
-/opt/pkg/bin/pip install --upgrade pip 
-/opt/pkg/bin/pip install sphinx jupyter pandas scipy sklearn matplotlib networkx deap django flask feather-format tox
-/opt/pkg/bin/pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.1-cp35-none-any.whl
+#pip install --upgrade pip 
+#pip install sphinx jupyter pandas scipy sklearn matplotlib uvloop http-tools networkx deap django flask feather-format tox
+#sudo /opt/pkg/bin/pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.1-cp35-none-any.whl
 
 mkdir ~/.matplotlib && echo "backend: qt4agg" >> ~/.matplotlib/matplotlibrc
 
-echo "PATH=/opt/pkg/bin:/opt/pkg/sbin:$PATH:/opt/pkg/qt4/bin" >> ~/.bashrc
-echo "PATH=/opt/pkg/bin:/opt/pkg/sbin:$PATH:/opt/pkg/qt4/bin" >> ~/.zshrc
 echo "Done."
